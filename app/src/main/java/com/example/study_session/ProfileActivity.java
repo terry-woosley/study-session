f3d56913eb28803c4d13642bd923f86f7abebd5e
Dylan Rongey <dylan.rongey@gmail.com>
2019-10-04 21:28:26 -0500

    Finished profile activity. Added button to navigate to main activity.

--- app/src/main/java/com/example/study_session/ProfileActivity.java
+++ app/src/main/java/com/example/study_session/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.example.study_session;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+
+
+
+public class ProfileActivity extends AppCompatActivity implements  View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
