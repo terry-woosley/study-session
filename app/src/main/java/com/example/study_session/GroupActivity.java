a747cd8c95c50bffbe3020ebdccda37afa63a662
Ronny Aretz <S536783@nwmissouri.edu>
2019-10-04 19:24:23 -0500

    changed and renamed the group view. Ronny Aretz

--- app/src/main/java/com/example/study_session/GroupActivity.java
+++ app/src/main/java/com/example/study_session/GroupActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GroupChatActivity extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_chat);
+        setContentView(R.layout.activity_group);
     }
 }
