b2d3978eb3c187007b2ebf74642ec1b7ebada93d
Terry Woosley <s526788@nwmissouri.edu>
2019-10-22 20:39:53 -0500

    add attritubes to model class Group.java

--- app/src/main/java/com/example/study_session/Group.java
+++ app/src/main/java/com/example/study_session/Group.java
@@ -1,4 +1,10 @@
 package com.example.study_session;
 
 public class Group {
+    String groupName;
+    String groupSchool;
+    Profile creator;
+    Float[] timesAvailable;
+    Profile[] members;
+    String subject;
 }
