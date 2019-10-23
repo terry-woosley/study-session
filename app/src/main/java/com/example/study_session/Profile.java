b397dccc51eb000d0d033f80b436a8e963635c7e
Terry Woosley <s526788@nwmissouri.edu>
2019-10-22 20:37:24 -0500

    add attributes to model class Profile.java

--- app/src/main/java/com/example/study_session/Profile.java
+++ app/src/main/java/com/example/study_session/Profile.java
@@ -1,4 +1,9 @@
 package com.example.study_session;
 
 public class Profile {
+    String name;
+    String school;
+    Group[] groups;
+    //Maybe consider changing the type to something more fitting
+    Float timesAvailable;
 }
