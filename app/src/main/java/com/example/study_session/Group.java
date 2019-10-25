da97cc9435a04e0d3b33a5567af6133d962b1ba4
Ronny Aretz <S536783@nwmissouri.edu>
2019-10-25 17:10:45 -0500

    Includes a createGroup function. Ronny Aretz

--- app/src/main/java/com/example/study_session/Group.java
+++ app/src/main/java/com/example/study_session/Group.java
@@ -1,5 +1,19 @@
 package com.example.study_session;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
 public class Group {
     String groupName;
     String groupSchool;
@@ -7,4 +21,44 @@ public class Group {
     Date[] timesAvailable;
     String[] members;
     String subject;
+
+    public Group(String groupName, String groupSchool, String creator, Date[] timesAvailable, String[] members, String subject){
+        this.groupName = groupName;
+        this.groupSchool = groupSchool;
+        this.creator = creator;
+        this.timesAvailable = timesAvailable;
+        this.members = members;
+        this.subject = subject;
+    }
+
+    public void addNewGroup(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> group = new HashMap<>();
+        group.put("groupName", groupName);
+        group.put("creator", creator);
+        group.put("subject", subject);
+        group.put("timesAvailable", timesAvailable);
+
+        db.collection("groups")
+                .add(group)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+    }
 }
--- app/src/main/java/com/example/study_session/GroupCreateActivity.java
+++ app/src/main/java/com/example/study_session/GroupCreateActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.study_session.data.model.LoggedInUser;
+
 public class GroupCreateActivity extends AppCompatActivity {
 
     Button createGroupBTN;
@@ -18,14 +20,15 @@ public class GroupCreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_create);
-        createGroupBTN = findViewById(R.id.createBTN);
-        groupNameET = findViewById(R.id.groupNameET);
-        groupSubject = findViewById(R.id.groupSubjectET);
-        meetingTime = findViewById(R.id.meetingTimeET);
+        createGroupBTN = (Button) findViewById(R.id.createGroupBTN);
+        groupNameET = (EditText) findViewById(R.id.groupNameET);
+        groupSubject = (EditText) findViewById(R.id.groupSubjectET);
+        meetingTime = (EditText) findViewById(R.id.meetingTimeET);
         createGroupBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Group group = new Group(groupNameET.getText().toString(),"", "",null,null,groupSubject.getText().toString());
+                group.addNewGroup();
             }
         });
     }
