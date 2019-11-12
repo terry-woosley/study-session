470dbaa4fca12b825f55ee93a1d042e7853bd04f
Terry Woosley <s526788@nwmissouri.edu>
2019-11-12 10:54:42 -0600

    update group create to instantiate timesAvailable

--- app/src/main/java/com/example/study_session/GroupCreateActivity.java
+++ app/src/main/java/com/example/study_session/GroupCreateActivity.java
@@ -3,6 +3,7 @@ package com.example.study_session;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,14 +18,34 @@ public class GroupCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_create);
 
+        final ArrayList<Date> timesAvailable = new ArrayList<>();
         Button createGroupBTN = findViewById(R.id.createGroupBTN);
+        Button addTimeBTN = findViewById(R.id.addTimeBTN);
         final EditText groupNameET = findViewById(R.id.groupNameET);
         final EditText groupSubjectET = findViewById(R.id.groupSubjectET);
+
+        addTimeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //reconvert values everytime addTimeBTN is clicked
+                EditText weekdayET = findViewById(R.id.weekdayTimeET);
+                EditText hourET = findViewById(R.id.hourTimeET);
+                String hourString = hourET.getText().toString();
+                Integer hour = Integer.parseInt(hourString);
+                EditText minuteET = findViewById(R.id.minuteTimeET);
+                String minuteString = minuteET.getText().toString();
+                Integer minute = Integer.parseInt(minuteString);
+                //parse date and add it to timesAvailable
+                Date date = new Date(weekdayET.getText().toString(), hour, minute);
+                timesAvailable.add(date);
+                Log.d("LOG", "Date " + date.dateToText() + " added to times available");
+            }
+        });
 
         createGroupBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Group group = new Group(groupNameET.getText().toString(),"", "", new ArrayList<Date>(), new ArrayList<String>(),groupSubjectET.getText().toString());
+                Group group = new Group(groupNameET.getText().toString(),"", "", timesAvailable, new ArrayList<String>(),groupSubjectET.getText().toString());
                 group.addNewGroup();
             }
         });
