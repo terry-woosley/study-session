537f929a98fd55bf64a3fda2ae39fa8a1812b5d5
David Curtis <46725522+davidbradlycurtis@users.noreply.github.com>
2019-11-26 15:28:01 -0600

    Added methods to retrieve user info in ProfileActivity

--- app/src/main/java/com/example/study_session/ProfileActivity.java
+++ app/src/main/java/com/example/study_session/ProfileActivity.java
@@ -1,39 +1,49 @@
 package com.example.study_session;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.study_session.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 
 public class ProfileActivity extends AppCompatActivity {
     private FirebaseUser user;
+    private FirebaseFirestore db;
     public static final int VIEW_PROFILE = 90;
+    private String school, username, email, uid;
+    private ArrayList<Date> availableTimes;
+    private ArrayList<String> groups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         user = FirebaseAuth.getInstance().getCurrentUser();
+        db = FirebaseFirestore.getInstance();
         if (user != null) {
             setContentView(R.layout.activity_profile);
             TextView schoolSet = (TextView)findViewById(R.id.profileSchoolDisplayTV);
             TextView nameSet = (TextView)findViewById(R.id.profileNameDisplayTV);
             TextView emailSet = (TextView)findViewById(R.id.profileEmailDisplayTV);
-
             Intent intent = getIntent();
-
-            String school = intent.getStringExtra("school");
-            String email = intent.getStringExtra("email");
-            String username = intent.getStringExtra("username");
+            populateProfie(intent);
 
 
-
+            emailSet.setText(email);
             nameSet.setText(username);
             schoolSet.setText(school);
         } else {
@@ -42,12 +52,13 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
-    public void populateProfie(){
-        //get name from database
-        //get email from database
-        //get profile pic from database
-        //get times from database
-        //get groups from database
+    public void populateProfie(Intent intent){
+        uid = intent.getStringExtra("uid");
+        username = intent.getStringExtra("username");
+        school = intent.getStringExtra("school");
+        email = user.getEmail();
+        getGroups();
+        getTimes();
     }
 
     public void showMain(View view) {
@@ -58,6 +69,46 @@ public class ProfileActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         setResult(LoginActivity.LOGOUT);
         finish();
+    }
+
+    public void getGroups(){
+        DocumentReference docRef = db.collection("users").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        groups = (ArrayList<String>) document.get("groups");
+                        Log.d("Retrieving Groups", "Group data: " + groups.toString());
+                    } else {
+                        Log.d("Retrieving Groups", "No such document");
+                    }
+                } else {
+                    Log.d("Retrieving Groups", "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    public void getTimes(){
+        DocumentReference docRef = db.collection("users").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        availableTimes = (ArrayList<Date>) document.get("timesAvailable");
+                        Log.d("Retrieving Times", "Time data: " + availableTimes.toString());
+                    } else {
+                        Log.d("Retrieving Times", "No such document");
+                    }
+                } else {
+                    Log.d("Retrieving Times", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 
 
